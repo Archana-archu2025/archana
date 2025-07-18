ee6ff18b56631c8ca9faced0e7d751def65954b2
Archana-archu2025 <archana97sri@gmail.com>
2025-07-18 16:01:19 +0530

    commit

---
+++ src/main/java/FileUtility.java
@@ -0,0 +1,17 @@
+
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+
+public class FileUtility {
+
+	public String getDataFromPropertiesFile(String key) throws Throwable
+	{
+		FileInputStream fis=new FileInputStream("C:\\Users\\Dell\\eclipse-workspace\\CRMframework\\configAppdata\\commondata.properties");
+		Properties pobj=new Properties();
+		pobj.load(fis);
+		String data=pobj.getProperty(key);
+		return data;
+	}
+}
---
+++ src/main/java/Utility/FileUtility.java
@@ -0,0 +1,9 @@
+package Utility;
+
+public class FileUtility {
+
+	public static void main(String[] args) {
+		System.out.println("archana");
+	}
+
+}
